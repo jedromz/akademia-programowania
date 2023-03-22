9fce8296f1cb366ed80408274586e54f8868afc0
Jędrzej Romankiewicz <romankiewicz.j@gmail.com>
2023-03-22 13:32:39 +0100

    refcator

--- Golang/zadania/reddit/fetcher/redditFetcher.go
+++ Golang/zadania/reddit/fetcher/redditFetcher.go
@@ -27,45 +27,61 @@ type RedditFetcher interface {
 }
 
 type MyFetcher struct {
-	Url  string
-	Data response
+	Url     string
+	Payload response
 }
 
 func (m *MyFetcher) Fetch(ctx context.Context) error {
 	logger, ok := ctx.Value("logger").(*log.Logger)
 	if !ok {
-		// Handle missing logger in context
 		return errors.New("logger not found in context")
 	}
-	req, err := http.NewRequestWithContext(ctx, "GET", m.Url, nil)
+	resp, err := makeRequest(ctx, m.Url)
 	if err != nil {
-		logger.Println(err)
-		return err
-	}
-	logger.Printf("Making request: %s %s\n", req.Method, req.URL)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		logger.Println(err)
 		return err
 	}
 	defer resp.Body.Close()
-	err = json.NewDecoder(resp.Body).Decode(&m.Data)
+	err = json.NewDecoder(resp.Body).Decode(&m.Payload)
 	if err != nil {
 		logger.Println(err)
 		return err
 	}
 	return nil
 }
-
+func makeRequest(ctx context.Context, url string) (*http.Response, error) {
+	logger, ok := ctx.Value("logger").(*log.Logger)
+	if !ok {
+		return nil, errors.New("logger not found in context")
+	}
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		logger.Println(err)
+		return nil, err
+	}
+	logger.Printf("Making request: %s %s\n", req.Method, req.URL)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		logger.Println(err)
+		return nil, err
+	}
+	return resp, nil
+}
 func (m *MyFetcher) Save(ctx context.Context, writer io.Writer) error {
 	logger, ok := ctx.Value("logger").(*log.Logger)
 	if !ok {
-		// Handle missing logger in context
 		return errors.New("logger not found in context")
 	}
-	logger.Printf("Saving data...")
-	for _, dto := range m.Data.Data.Children {
-		_, err := fmt.Fprintf(writer, "%s\n%s\n\n", dto.Data.Title, dto.Data.URL)
+	logger.Printf("Saving data from %s", m.Url)
+	err := writeToFile(m, writer, logger)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeToFile(m *MyFetcher, writer io.Writer, logger *log.Logger) error {
+	for _, resp := range m.Payload.Data.Children {
+		_, err := fmt.Fprintf(writer, "%s\n%s\n\n", resp.Data.Title, resp.Data.URL)
 		if err != nil {
 			logger.Println(err)
 			return err
--- Golang/zadania/reddit/main.go
+++ Golang/zadania/reddit/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -17,8 +18,35 @@ const (
 	TargetDir = "target/"
 )
 
+func processSubreddit(ctx context.Context, subreddit string) error {
+	logger, ok := ctx.Value("logger").(*log.Logger)
+	if !ok {
+		return errors.New("logger not found in context")
+	}
+	filename := strings.TrimSuffix(path.Base(subreddit), ".json")
+	myFetcher := fetcher.MyFetcher{Url: Path + subreddit}
+	file, err := os.Create(TargetDir + filename)
+	if err != nil {
+		logger.Printf("failed to create file: %v", err)
+		return err
+	}
+	defer file.Close()
+
+	err = myFetcher.Fetch(ctx)
+	if err != nil {
+		logger.Printf("failed to fetch data for %s: %v", subreddit, err)
+		return err
+	}
+	err = myFetcher.Save(ctx, file)
+	if err != nil {
+		logger.Printf("failed to save data for %s: %v", subreddit, err)
+		return err
+	}
+	return nil
+}
 func main() {
 	fmt.Println("hello r/golang!")
+
 	ctx := context.Background()
 	logger := log.New(os.Stdout, "[log] ", log.LstdFlags)
 	ctx = context.WithValue(ctx, "logger", logger)
@@ -39,24 +67,9 @@ func main() {
 	for _, v := range subreddits {
 		go func(subreddit string) {
 			defer wg.Done()
-			filename := strings.TrimSuffix(path.Base(subreddit), ".json")
-			myFetcher := fetcher.MyFetcher{Url: Path + subreddit}
-			file, err := os.Create(TargetDir + filename)
+			err := processSubreddit(ctx, subreddit)
 			if err != nil {
-				logger.Printf("failed to create file: %v", err)
-				return
-			}
-			defer file.Close()
-
-			err = myFetcher.Fetch(ctx)
-			if err != nil {
-				logger.Printf("failed to fetch data for %s: %v", subreddit, err)
-				return
-			}
-			err = myFetcher.Save(ctx, file)
-			if err != nil {
-				logger.Printf("failed to save data for %s: %v", subreddit, err)
-				return
+				log.Println(err)
 			}
 		}(v)
 	}
